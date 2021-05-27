2896f14e67061b5c81cd335e699771d78a97b5a5
libin <807242867@qq.com>
2021-05-27 16:35:41 +0800

    bushu

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import styleImport from 'vite-plugin-style-import'
 export default defineConfig({
+  base: "/QRCode-creator-web/",
   plugins: [
     vue(),
     styleImport({
